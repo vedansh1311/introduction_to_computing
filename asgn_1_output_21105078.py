c6bbf8f734beb2c345a2b9f0e79e5a42d80acf0c
vedansh1311 <97685317+vedansh1311@users.noreply.github.com>
2022-01-14 12:35:54 +0530

    Add files via upload

---
+++ asgn_1_output_21105078.py
@@ -0,0 +1,42 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:/Users/HP/Desktop/asgn_1_21105078.py ===============
+Enter first number:-
+58
+Enter second number:-
+56
+Enter third number:-
+23
+Average of the three numbers is:-
+ 45.666666666666664
+Enter your gross income in dollars
+200000000
+Enter number of dependents
+42
+Your total tax:
+ 3997280.0
+Enter your student id:
+21105078
+Enter your name:
+vedansh singh
+Enter your gender(use M for male,F for female,U for unknown):
+m
+Enter your course name:
+intro to computing
+Enter your CGPA:
+10
+[21105078, 'vedansh singh', 'm', 'intro to computing', 10.0]
+Enter marks of first student:
+100
+Enter marks of second student:
+25
+Enter marks of third student:
+15
+Enter marks of forth student:
+69
+Enter marks of fifth student:
+36
+[15.0, 25.0, 36.0, 69.0, 100.0]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
