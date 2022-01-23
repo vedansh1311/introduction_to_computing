6ed7de25ce0271ad5e1221c4410f51d8a77ef41d
vedansh1311 <97685317+vedansh1311@users.noreply.github.com>
2022-01-23 15:05:34 +0530

    Add files via upload

---
+++ ASSIGNMENT_2_output_SID_21105078.py
@@ -0,0 +1,38 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\HP\AppData\Local\Programs\Python\Python310\phython\2.py.py =
+Enter the string : "Phython is a case sensitive language"
+The length of the given string is : 38
+The string in reverse would be : "egaugnal evitisnes esac a si nohtyhP"
+The new string becomes :  a case sensitive 
+The replaced substring will be : object oriented
+The first occurence of the given substring "a" is at index no. = 12
+The given input string with no white spaces will be ""Phythonisacasesensitivelanguage""
+
+Enter your name : VEDANSH SINGH
+Enter your SID : 21105078
+Enter the name of your department : ECE
+Enter your CGPA : 10
+
+Hey, I Am VEDANSH SINGH Here!
+My SID is 21105078
+I am from ECE department and my CGPA is 10.000000
+
+a.	a & b = 8
+b.	a | b = 58
+c.	a ^ b = 50
+d.	Left shift both a and b with 2 bits : a = 224, b = 40
+e.	Right shift a with 2 bits and b with 4 bits : a = 14, b = 0
+
+Enter first number : 3105
+Enter second number : 456
+Enter third number : 1236
+The greatest number is 3105
+
+Enter the string : I AM FROM LUCKNOW
+No
+
+Enter the dimensions to check whether triangle possible or not : 35 45 85
+No
+
