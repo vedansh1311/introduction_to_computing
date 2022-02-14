16a7d5e54fca8aeae355aeeac91a26b37fc290c6
vedansh1311 <97685317+vedansh1311@users.noreply.github.com>
2022-02-14 21:55:12 +0530

    Add files via upload

---
+++ 21105078_output_assm3.py
@@ -0,0 +1,73 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\HP\Desktop\phython assignment\21105078_assgnment_3.py ===
+Enter the string: vedansh is vibhu and vibhu is vedansh
+The occurence(s) of:
+the word ' vedansh ' occurred 2  times
+the word ' is ' occurred 2  times
+the word ' vibhu ' occurred 2  times
+the word ' and ' occurred 1  times
+
+Enter the day 28
+Enter the month13
+enter the year2014
+Please use the given conditions for entering the current date
+C1 : 1<=month<=12
+C2 : 1<=day<=31
+C3 : 1800<=year<=2025
+
+Enter the day 28
+Enter the month02
+enter the year 2014
+The next date is [yyyy-mm-dd] 1-3-2014.
+
+Enter number of elements : 4
+16
+17
+5
+4
+[(16, 256), (17, 289), (5, 25), (4, 16)]
+
+enter your gpa 9
+Your grade is ' A ' and Very Good performance
+
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+Enter the name of the student: vedansh singh
+Enter the SID of vedansh singh: 21105078
+
+You have entered 1 value(s) till now
+Do you want to enter more data? no
+
+a. Student Details:
+The SID vedansh singh is 21105078
+
+b. Student Details (sorted with respect to names):
+The SID of vedansh singh is 21105078
+
+c. Student Details (sorted with respect to SIDs):
+The SID of vedansh singh is 21105078
+
+d. Enter the SID of the student: 21105078
+The name of student whose SID is 21105078 is vedansh singh
+
+ How many terms? 7
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+average 2.857142857142857
+a) {1, 3, 5, 6, 8}
+b) {3, 6, 8, 9, 13, 17}
+c) {1, 2, 4, 5}
+d) {6, 7, 8, 9, 10}
