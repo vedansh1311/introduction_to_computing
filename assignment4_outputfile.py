6e635cdb1c103475d6c08c39af07f9325edc9857
vedansh1311 <97685317+vedansh1311@users.noreply.github.com>
2022-03-01 22:37:46 +0530

    Add files via upload

---
+++ assignment4_outputfile.py
@@ -0,0 +1,102 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================================================== RESTART: C:/Users/HP/Desktop/phython assignment/assignment4_source_file.py ======================================================
+Q1
+Enter number of disk in tower of Hanoi:5
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Move disk 4 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 2 from Destination to Source Tower
+Move disk 1 from Auxiliary to Source Tower
+Move disk 3 from Destination to Auxiliary
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 5 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Move disk 3 from Auxiliary to Source Tower
+Move disk 1 from Destination to Auxiliary
+Move disk 2 from Destination to Source Tower
+Move disk 1 from Auxiliary to Source Tower
+Move disk 4 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Q2
+enter the no. of rows6
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+Q3
+
+Enter an Integer:50
+Enter another Integer:25
+
+The quotient when 50 is divided by 25 is 2.
+The remainder when 50 is divided by 25 is 0.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All result values are NOT 'non-zero'
+
+Que3.c
+Appended (4,5,6) in the values (2,0)
+Values greater than 4 is 5,6
+
+Que3.d
+Above result in set form is shown below:
+{5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 6
+Hash value of 6 is 6
+Q4
+enter name of student :vedansh singh
+Enter SID of vedansh singh: 21105078
+object created
+
+The name of the student is vedansh singh and his/her roll number is 21105078
+object deleted
+Q5
+The data is stored where:
+Mehak has a salary of 40000 rupees
+Ashok has a salary of 50000 rupees
+Viren has a salary of 60000 rupees
+All deleted, i.e. Viren
+
+Q6
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:vibhu
+Enter Player2 name:vedansh
+
+Enter Word by vibhu:algoritham
+Enter Word by vedansh:logaritham
+
+Friendship of vibhu and vedansh is REAL
